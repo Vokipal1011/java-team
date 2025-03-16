36e05ae1812eac4616079c9b008994f2482c32f1
Степан Лапиков <vokipal@yandex.ru>
2025-03-16 11:24:09 +0300

    первый коммит - добавлен Spring Boot проект

---
+++ src/test/java/com/ourcompany/javateam/JavaTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourcompany.javateam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
