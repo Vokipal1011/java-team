bc914835b0cca71e83239c26811ce9fbde476502
Степан Лапиков <vokipal@yandex.ru>
2025-03-16 11:38:14 +0300

    добавил код с Hello World и закомментировал старт Spring Boot

--- src/main/java/com/ourcompany/javateam/JavaTeamApplication.java
+++ src/main/java/com/ourcompany/javateam/JavaTeamApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaTeamApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaTeamApplication.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(JavaTeamApplication.class, args);
+//    }
 
+    public static void main(String[] args) {
+        System.out.println("Hello word - Степан Лапиков");
+    }
 }
